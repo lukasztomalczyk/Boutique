77e6b7e6d15a46208190e995edf5c33234b25b53
garibo <Garibo@o2.pl>
2018-06-23 21:04:57 +0200

    RegisteredCommand refaktor

--- services/ShopOnline/Boutique.Presentation/Commands/Auth/RegisterCommand.cs
+++ services/ShopOnline/Boutique.Presentation/Commands/Auth/RegisterCommand.cs
@@ -6,16 +6,14 @@ namespace Boutique.Presentation.Commands.Auth
 {
     public class RegisterCommand
     {
-        public string Id { get; }
         public string Login { get; }
         public string Password { get; }
         public string FirstName { get; }
         public string LastName { get; }
         public string Role { get; } 
 
-        public RegisterCommand(string id, string login, string password, string firstName, string lastName, string role)
+        public RegisterCommand(string login, string password, string firstName, string lastName, string role)
         {
-            Id = id;
             Login = login;
             Password = password;
             FirstName = firstName;
