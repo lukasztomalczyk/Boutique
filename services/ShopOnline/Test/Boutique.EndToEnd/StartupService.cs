451b98012d6924b6d6191767f7c8280e4e630a39
garibo <Garibo@o2.pl>
2018-08-12 15:38:26 +0200

    budowanie bazy danych - testy

---
+++ services/ShopOnline/Test/Boutique.EndToEnd/StartupService.cs
@@ -0,0 +1,19 @@
+﻿using DatabaseDeploy;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boutique.EndToEnd
+{
+    [SetUpFixture]
+    public class StartupService 
+    {
+        [OneTimeSetUp]
+        public void DatabaseDeploy()
+        {
+           // Program.Main(new[] { @"..\..\..\FileStore\Boutique.Database.dacpac" });
+        }
+    }
+}
