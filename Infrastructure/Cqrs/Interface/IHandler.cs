873e5826672d3b51cc0a1b8b8d0a90d248db8ed2
garibo <Garibo@o2.pl>
2018-09-02 12:51:50 +0200

    Refaktor - created abstarction for listener and handler

---
+++ Infrastructure/Cqrs/Interface/IHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cqrs.Interface
+{
+    public interface IListenerHandler
+    {
+        void Handle(string message);
+    }
+}
