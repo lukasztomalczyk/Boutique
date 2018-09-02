f28a71537d4a8976bd90b2a341b8fbfc1338a8a6
garibo <Garibo@o2.pl>
2018-09-02 14:01:51 +0200

    Appliton StartUp was add log critical when application stop

--- EventSourceScheduler/Program.cs
+++ EventSourceScheduler/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace EventSourceScheduler
 {
@@ -19,6 +20,7 @@ namespace EventSourceScheduler
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseNLog()
                 .UseStartup<Startup>();
     }
 }
--- EventSourceScheduler/Startup.cs
+++ EventSourceScheduler/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 using RabbitMQ.ServicesCollection;
 using System.Threading;
 
@@ -13,7 +15,7 @@ namespace EventSourceScheduler
     {
         public IConfiguration Configuration { get; }
         public CancellationToken CancellationToken { get; set; }
-
+        public ILogger Logger { get; set; }
         public Startup(IConfiguration configuration,IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -29,25 +31,33 @@ namespace EventSourceScheduler
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime)
+        public void Configure(ILoggerFactory loggerFactory, IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime)
         {
+            //TODO to refaktor ApplicaitonStoped
+            Logger = loggerFactory.CreateLogger<Startup>();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseMvc();
+            loggerFactory.AddNLog();
+
             lifetime.ApplicationStopped.Register(ApplicaitonStoped);
 
             app.UseEventSourceListener(CancellationToken);
         }
 
+        //TODO to refaktor
         private void ApplicaitonStoped()
         {
             var tokenSource = new CancellationTokenSource();
             tokenSource.Cancel();
 
             CancellationToken = tokenSource.Token;
+
+            Logger.LogCritical("Service was stopped");
         }
     }
 }
