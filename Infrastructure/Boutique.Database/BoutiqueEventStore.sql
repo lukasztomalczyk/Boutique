35c804bf4a4bea4e2a2628840dfc84ba4659df72
garibo <Garibo@o2.pl>
2018-09-02 12:59:21 +0200

    Revert database projket and was added new table EvetStore

---
+++ Infrastructure/Boutique.Database/BoutiqueEventStore.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[BoutiqueEventStore]
+(
+	[Id] INT NOT NULL,
+	[Type] VARCHAR(50) NOT NULL,
+	[AdditionalData] NVARCHAR(MAX) NOT NULL,
+	[CreateAt] DATETIME2(2) NOT NULL,
+
+	CONSTRAINT [PK_BoutiqueEventStore_Id] PRIMARY KEY CLUSTERED  ([Id] DESC)
+)
+ON [FG_PrimaryKey] TEXTIMAGE_ON [FG_Text]
