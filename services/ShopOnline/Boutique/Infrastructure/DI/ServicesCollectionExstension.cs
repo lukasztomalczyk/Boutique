f0f4d4a89205d6047aef2bdeda3ef49254d1a088
garibo <Garibo@o2.pl>
2018-09-15 10:02:58 +0200

    Dodano FluentValidation - Command Handler Valid DI - inject Validators

---
+++ services/ShopOnline/Boutique/Infrastructure/DI/ServicesCollectionExstension.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Boutique.Infrastructure.DI
+{
+    public static class ServicesCollectionExstension
+    {
+        public static void AddCommandValidators(this IServiceCollection services,params Assembly[] assemblies)
+        {
+            services.Scan(p => p.FromAssemblies(assemblies)
+            .AddClasses(a => a.AssignableTo(typeof(IValidator<>)))
+            .AsImplementedInterfaces()
+            .AsSelf()
+            .WithScopedLifetime());
+        }
+    }
+}
